012f00ee2a69946a8dca43cd7ff893adfd6a9cf2
Tristan <hjpark002@gmail.com>
2025-01-23 20:44:31 +0900

    Add authentication using Clerk

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,17 +2,48 @@
 import { Button, Navbar, TextInput } from 'flowbite-react';
 import Link from 'next/link';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { usePathname } from 'next/navigation';
+import { useTheme } from 'next-themes';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { dark, light } from '@clerk/themes';
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
 
 export default function Header() {
+	const path = usePathname();
+	const { theme, setTheme } = useTheme();
+	const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchParams = useSearchParams();
+
+	const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(searchParams);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    router.push(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(searchParams);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [searchParams]);
+	
 	return (
 		<Navbar className='border-b-2'>
 			<Link
 				href='/'
 				className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
 			>
-				<span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-blue-700 to-blue-900 rounded-lg text-white tracking-tighter'>
-					Inko Jaya Konstruksi
+				<span className='px-2 py-1 bg-gradient-to-r from-blue-700 via-blue-800 to-blue-900 rounded-lg text-white tracking-tighter'>
+					Inko Jaya
 				</span>
+        <span className='tracking-tight'>&nbsp;Konstruksi</span>
 			</Link>
 			<form>
 				<TextInput
@@ -20,11 +51,61 @@ export default function Header() {
 					placeholder='Search...'
 					rightIcon={AiOutlineSearch}
 					className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
 				/>
 			</form>
 			<Button className='w-12 h-10 lg:hidden' color='gray' pill>
 				<AiOutlineSearch />
 			</Button>
+			<div className='flex gap-2 md:order-2'>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        </Button>
+        <SignedIn>
+          <UserButton
+            appearance={{
+              baseTheme: theme === 'light' ? light : dark,
+            }}
+            userProfileUrl='/dashboard?tab=profile'
+          />
+        </SignedIn>
+        <SignedOut>
+          <Link href='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        </SignedOut>
+				<Navbar.Toggle />
+      </div>
+			<Navbar.Collapse>
+        <Link href='/'>
+          <Navbar.Link active={path === '/'} as={'div'}>
+            Home
+          </Navbar.Link>
+        </Link>
+        <Link href='/about'>
+          <Navbar.Link active={path === '/about'} as={'div'}>
+            About
+          </Navbar.Link>
+        </Link>
+        <Link href='/services'>
+          <Navbar.Link active={path === '/services'} as={'div'}>
+            Services & Products
+          </Navbar.Link>
+        </Link>
+        <Link href='/projects'>
+          <Navbar.Link active={path === '/projects'} as={'div'}>
+            Projects
+          </Navbar.Link>
+        </Link>
+      </Navbar.Collapse>
 		</Navbar>
 	);
 }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,10 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from './components/Header';
+import { ThemeProvider } from 'next-themes';
+import ThemeCom from './components/ThemeCom';
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeModeScript } from 'flowbite-react';
 
 
 const geistSans = Geist({
@@ -20,13 +24,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <ThemeModeScript />
+        </head>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider>
+            <ThemeCom>
+              <Header />
+              {children}
+            </ThemeCom>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
